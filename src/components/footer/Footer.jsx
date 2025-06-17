a2a28274775a5ab21a92169e3fb171187e37cb32
Raymov <googlaccnew12@gmil.com>
2025-06-18 03:12:21 +0500

    update: home page new section and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../../../public/logoAoron.png";
+import { NavLink } from "react-router-dom";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <footer >
+        <div className="pt-10 bg-gray-200">
+          <div className="container m-auto grid grid-cols-4 gap-8">
+            <div className="space-y-4">
+              <div className="w-20 mb-4">
+                <img src={logo} alt="logo" />
+              </div>
+              <p className="text-sm text-gray-500 text-muted-foreground ">
+                Premium quality menswear focused on exceptional materials and
+                perfect fit.
+              </p>
+            </div>
+            <div>
+              <h4 className="font-medium text-sm uppercase tracking-wider">
+                Shop
+              </h4>
+              <ul className="flex mt-1.5 flex-col gap-1.5">
+                <li className="text-gray-500">
+                  {<NavLink to={"/catalog"}>View All Products</NavLink>}
+                </li>
+                <li className="text-gray-500">
+                  {<NavLink to={"/catalog"}>Suits</NavLink>}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4 className="font-medium text-sm uppercase tracking-wider">
+                Company
+              </h4>
+              <ul className="flex mt-1.5 flex-col gap-1.5">
+                <li className="text-gray-500">
+                  {<NavLink to={"/about"}>About</NavLink>}
+                </li>
+                <li className="text-gray-500">
+                  {<NavLink to={"/contact"}>Contact</NavLink>}
+                </li>
+                <li className="text-gray-500">
+                  {<NavLink to={"/news"}>Privacy Policy</NavLink>}
+                </li>
+                <li className="text-gray-500">
+                  {<NavLink to={"/terms"}>Delivery terms</NavLink>}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4 className="font-medium text-sm uppercase tracking-wider">
+                Subscribe to our newsletter
+              </h4>
+              <p className="text-sm my-2.5 text-gray-500 text-muted-foreground">
+                Subscribe to receive updates, access to exclusive deals, and
+                more.
+              </p>
+              <div className="w-full max-w-md sm:max-w-lg lg:max-w-xl flex">
+                <input
+                  type="email"
+                  className="w-full px-3 py-1 sm:text-base border border-r-0 rounded-l-md focus:outline-none focus:ring-1 focus:ring-primary"
+                />
+                <button className="flex py-1 items-center justify-center bg-gray-700 text-primary-foreground px-4  rounded-r-md hover:bg-primary/90 transition-colors">
+                  -
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="container m-auto py-4 bg-gray-200">< hr />
+          <p className="text-xs mt-4 text-gray-500 text-muted-foreground">© 2025 AORON. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,266 @@
-import React from 'react'
-import heroimg from "./img/hero.jpg"
+import React, { useEffect, useState } from "react";
+import heroimg from "./img/hero.jpg";
+import { API } from "../../api/API";
+import { NavLink } from "react-router-dom";
+import img1 from "./img/img1.jpeg"
 export default function Home() {
-  const style_one ={
+  const [response, setResponse] = useState([]);
+  const getApi = async () => {
+    try {
+      const res = await API.get("/product");
+      setResponse(res.data?.data?.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+  useEffect(() => {
+    getApi();
+  }, []);
+  const style_one = {
     backgroundImage: `url(${heroimg})`,
-      backgroundPosition: 'center 30%',
-  }
+    backgroundPosition: "center 30%",
+  };
   return (
     <>
-    <section className='relative h-screen flex items-center overflow-hidden mt-3'>
-<div className='absolute inset-0 w-full h-full bg-cover bg-center' style={style_one}>1</div>
-    <div className='container mx-auto px-4 relative z-10'>
-      <div className='max-w-2xl'>
-        <div className='flex flex-col items-start space-y-8 animate-fade-up'>
-          <span className='inline-block bg-white/10 backdrop-blur-sm px-3 py-1 text-xs tracking-wider uppercase text-white'>Spring/Summer 2025</span>
-          <h1 className='text-5xl md:text-6xl lg:text-7xl font-light tracking-tight text-white'>Premium Menswear</h1>
-        <p className='text-lg md:text-xl text-white/90 max-w-lg'>Quality fabrics. Perfect fit. Timeless style.</p>
-        <a className='group bg-white text-primary px-6 py-3 rounded-md inline-flex items-center hover:bg-white/90 transition-colors' href="/catalog">Explore Collection</a>
+      <section className="relative h-screen flex items-center overflow-hidden mt-3">
+        <div
+          className="absolute inset-0 w-full h-full bg-cover bg-center"
+          style={style_one}
+        >
+          1
         </div>
-      </div>
-    </div>
-    </section>
-    <div>Home page</div>
+        <div className="container mx-auto px-4 relative z-10">
+          <div className="max-w-2xl">
+            <div className="flex flex-col items-start space-y-8 animate-fade-up">
+              <span className="inline-block bg-white/10 backdrop-blur-sm px-3 py-1 text-xs tracking-wider uppercase text-white">
+                Spring/Summer 2025
+              </span>
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-light tracking-tight text-white">
+                Premium Menswear
+              </h1>
+              <p className="text-lg md:text-xl text-white/90 max-w-lg">
+                Quality fabrics. Perfect fit. Timeless style.
+              </p>
+              <a
+                className="group bg-white text-primary px-6 py-3 rounded-md inline-flex items-center hover:bg-white/90 transition-colors"
+                href="/catalog"
+              >
+                Explore Collection
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section-container m-16">
+        <div className="mb-10 text-center">
+          <h2 className="heading-lg mb-4">Featured Products</h2>
+          <p className="text-muted-foreground text-gray-600 max-w-2xl mx-auto">
+            Discover our carefully selected collection of premium menswear,
+            crafted with the finest materials and attention to detail.
+          </p>
+        </div>
+        <div className="m-auto product-grid gap-8 grid grid-cols-4 container">
+          {response.map((item) => (
+            <>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={`https://testaoron.limsa.uz/${item?.images}`}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">{item?.title_en}</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">${item?.price}</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">{item?.description_en}</p>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+        <div className="mt-12 text-center">
+          
+        {<NavLink to={"/catalog"}><div className="border border-gray-300 rounded-md py-3 px-6 btn-secondary group inline-flex items-center">View All Products</div></NavLink>}
+        </div>
+      </section>
+      <section className="section-container my-16">
+<div className="mb-10 container m-auto">
+  <div className="flex justify-between items-end my-10">
+    <h2 className="text-xl">New Arrivals</h2>
+    {<NavLink to={"/catalog"}>View All Products</NavLink>}
+  </div>
+  <div className="m-auto product-grid gap-8 grid grid-cols-4 container">
+  <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+  </div>
+</div>
+      </section>
+      <section className="py-16 bg-red-200">
+        <div className="container m-auto">
+<div className="flex justify-between items-end my-10">
+  <h2 className="text-xl text-red-400">Sale</h2>
+  {<NavLink to={"/catalog"}>View All Products</NavLink>}
+</div>
+<div className="m-auto product-grid gap-8 grid grid-cols-4 container">
+  <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              <div>
+                <div className="h-[300px]">
+                  <img
+                    className="h-[300px] w-full object-cover"
+                    src={img1}
+                    alt="image"
+                  />
+                </div>
+                <div className="mt-4 space-y-1">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-sm font-medium">
+Men’s classic 3-piece suit in solid dark grey – understated, elegant, and versat</h3>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm font-medium">$43</span>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600 text-muted-foreground line-clamp-2">A timeless 3-piece men’s suit in solid dark grey — a versatile and essential option for office wear, business meetings, and formal occasions. The neutral tone and structured slim fit make it easy to pair with any shirt or tie, helping you achieve a confident and elegant look</p>
+                </div>
+              </div>
+              </div>
+</div>
+      </section>
     </>
-  )
+  );
 }
