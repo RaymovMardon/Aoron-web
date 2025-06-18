82b8db5fd5347aa8da43c835e7a81792b8ba63e8
abdusattorovJaloliddin <jaloliddinabdusattorov2005>
2025-06-18 12:24:21 +0500

    Catalog

---
+++ src/pages/catalog/Catalog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Catalog() {
+  return (
+    <section className="container mx-auto px-4 mt-58">
+      <div>
+        <h1 className="text-3xl font-bold mb-6">Our collection</h1>
+        <p className="text-gray-700 mb-4">
+          Browse our collection of premium menswear, designed with quality and
+          style in mind..
+        </p>
+      </div>
+    </section>
+  );
+}
